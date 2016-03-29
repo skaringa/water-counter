d7d7e1b058832581a83c36f2e1bae21b0606b206
Martin Kompf <mkompf@chronos>
2016-03-29 19:22:28 +0200

    print daily values to graph

--- graph.sh
+++ graph.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 rrdtool graph counter.gif \
-  -s 'now -20 hour' -e 'now' \
+  -s 'now -1 day' -e 'now' \
   -X 0 -A \
   DEF:counter=water.rrd:counter:LAST \
+  VDEF:lastcount=counter,LAST \
+  "GPRINT:lastcount:%6.3lf m³" \
   LINE2:counter#000000:"Zählerstand [m³]"
 display counter.gif&
 rrdtool graph consum.gif \
-  -s 'now -20 hour' -e 'now' \
+  -s 'now -1 day' -e 'now' \
   DEF:consum=water.rrd:consum:AVERAGE \
   CDEF:consumltr=consum,60000,* \
+  CDEF:conpd=consumltr,60,*,24,* \
+  VDEF:conpdtotal=conpd,AVERAGE \
+  "GPRINT:conpdtotal:Total %4.0lf l/d" \
   LINE2:consumltr#00FF00:"Verbrauch [l/min]" 
 display consum.gif&
